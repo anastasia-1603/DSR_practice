21d885143cb513c999426141800f881b53a69f1b
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2022-08-20 15:28:46 +0300

    solved the problem n+1 for Item

--- src/main/java/com/example/practice/repository/ItemRepository.java
+++ src/main/java/com/example/practice/repository/ItemRepository.java
@@ -2,6 +2,9 @@ package com.example.practice.repository;
 
 import com.example.practice.entity.Category;
 import com.example.practice.entity.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -11,4 +14,8 @@ import java.util.List;
 public interface ItemRepository extends JpaRepository<Item, Long>, JpaSpecificationExecutor<Item> {
 
     List<Item> findAllByCategory(Category category);
+
+    @Override
+    @EntityGraph(attributePaths = {"category", "user"})
+    Page<Item> findAll(Pageable pageable);
 }
